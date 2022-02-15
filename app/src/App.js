6a2f755973be7cad280bfa0114336b44ae604747
Olayiwola Olanrewaju <larry_coal@outlook.com>
2022-02-14 22:06:45 -0500

    first draft dapp

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,65 @@
-import React from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
+import CandyMachine from "./CandyMachine";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [userWalletAddress, setUserWallet] = useState(null);
+  const checkIfSolana = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana && solana.isPhantom) {
+        console.log("Phantom wallet found");
+
+        const response = await solana.connect({ onlyIfTrusted: true });
+
+        console.log("solana address", response.publicKey.toString());
+        setUserWallet(response.publicKey.toString());
+      } else {
+        alert("Please download phantom solana wallet");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfSolana();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    const response = await solana.connect();
+    setUserWallet(response.publicKey.toString());
+  };
+
+  const renderConnectWalletButton = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
+          <p className="header">🍭 The Goat</p>
+          <p className="sub-text">NFT drop for the goat</p>
+          {!userWalletAddress && renderConnectWalletButton()}
+          {userWalletAddress && <CandyMachine walletAddress={window.solana} />}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+// ts-node ~/metaplex/js/packages/cli/src/candy-machine-v2-cli.ts update_candy_machine -e devnet  -k ~/.config/solana/devnet.json -cp config.json
